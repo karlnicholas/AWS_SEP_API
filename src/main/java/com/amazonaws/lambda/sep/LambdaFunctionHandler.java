dd084dd133b20e25092a25c111f7820695d19ea6
karlnicholas <karl.nicholas@outlook.com>
2017-03-27 15:54:16 -0700

    Updates request and response.

--- src/main/java/com/amazonaws/lambda/sep/LambdaFunctionHandler.java
+++ src/main/java/com/amazonaws/lambda/sep/LambdaFunctionHandler.java
@@ -10,24 +10,35 @@ import java.nio.charset.StandardCharsets;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser.Feature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import quote.GetQuote;
+import sep.lucene.SearchResult;
 
 
 public class LambdaFunctionHandler implements RequestStreamHandler {
 	//create ObjectMapper instance
-	private ObjectMapper objectMapper = new ObjectMapper();
+	private ObjectMapper objectMapper;
 	private GetQuote getQuote = new GetQuote();
 	
-
+	public LambdaFunctionHandler() {
+		objectMapper = new ObjectMapper();
+		objectMapper.configure(Feature.ALLOW_SINGLE_QUOTES, true);
+	}
+	
 	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
 		RequestCommand command;
 		try {
 			command = objectMapper.readValue(input, RequestCommand.class);
 			if ( command.action != null && command.action.toLowerCase().trim().equals("quote")) {
-				output.write(getQuote.getEntryFromStanford().preamble.getBytes(StandardCharsets.UTF_8));
+				QuoteResponse response = new QuoteResponse();
+				SearchResult searchResult = getQuote.getEntryFromStanford();
+				response.subject = searchResult.subject; 
+				response.preamble = searchResult.preamble; 
+				response.url = searchResult.url; 
+				objectMapper.writeValue(output, response);
 			} else {
 				objectMapper.writeValue(output, command);
 			}
@@ -48,7 +59,8 @@ public class LambdaFunctionHandler implements RequestStreamHandler {
 	
 	public static void main(String... strings) throws Exception {
 //		String input = "{\"action\":\"quote\", \"query\":\"frege\"}";
-		String input = "{\"action\":\"quote\"}";
+//		String input = "{\"action\":\"quote\"}";
+		String input = "{'action':'quote'}";
 		ByteArrayOutputStream output = new ByteArrayOutputStream(); 
 		new LambdaFunctionHandler().handleRequest( 
 			new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)), 
@@ -67,4 +79,13 @@ public class LambdaFunctionHandler implements RequestStreamHandler {
 		}
 	}
 		
+	public static class QuoteResponse {
+		public String subject;
+		public String preamble;
+		public String url;
+		@Override
+		public String toString() {
+			return "[" + subject + ", " + preamble + ", " + url + "]";
+		}
+	}
 }
