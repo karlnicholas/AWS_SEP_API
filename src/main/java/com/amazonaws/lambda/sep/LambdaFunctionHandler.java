c9485c4270b51fbf7b050906ed59b59d8c34a665
karlnicholas <karl.nicholas@outlook.com>
2017-03-30 10:19:51 -0700

    Added test cases.

--- src/main/java/com/amazonaws/lambda/sep/LambdaFunctionHandler.java
+++ src/main/java/com/amazonaws/lambda/sep/LambdaFunctionHandler.java
@@ -6,15 +6,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.lucene.queryparser.classic.ParseException;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser.Feature;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import quote.GetQuote;
+import sep.lucene.SearchFiles;
 import sep.lucene.SearchResult;
 
 
@@ -22,26 +30,69 @@ public class LambdaFunctionHandler implements RequestStreamHandler {
 	//create ObjectMapper instance
 	private ObjectMapper objectMapper;
 	private GetQuote getQuote = new GetQuote();
+    private SearchFiles searchFiles = new SearchFiles();
 	
 	public LambdaFunctionHandler() {
 		objectMapper = new ObjectMapper();
 		objectMapper.configure(Feature.ALLOW_SINGLE_QUOTES, true);
 	}
 	
+    @SuppressWarnings("unchecked")
 	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
-		RequestCommand command;
 		try {
-			command = objectMapper.readValue(input, RequestCommand.class);
-			if ( command.action != null && command.action.toLowerCase().trim().equals("quote")) {
-				QuoteResponse response = new QuoteResponse();
+            Map<String, Object> request = objectMapper.readValue(input, new TypeReference<Map<String, Object>>() {});
+
+			Map<String, Object> params = (Map<String, Object>)request.get("params");
+            Map<String, Object> querystring = (Map<String, Object>)params.get("querystring");
+            String action = (String)querystring.get("action");
+			QuoteResponse response = new QuoteResponse();
+			if ( action != null && action.toLowerCase().trim().equals("quote")) {
 				SearchResult searchResult = getQuote.getEntryFromStanford();
 				response.subject = searchResult.subject; 
 				response.preamble = searchResult.preamble; 
 				response.url = searchResult.url; 
+				if (response.preamble.length() > 290 ) {
+					response.preamble = response.preamble.substring(0,  290); 					
+				}
 				objectMapper.writeValue(output, response);
+			} else if ( action != null && action.toLowerCase().trim().equals("search")) {
+	            String phrase = (String)querystring.get("phrase");
+		        if (phrase == null || phrase.isEmpty()) {
+					response.subject = "No Search Phrase"; 
+					response.preamble = "No Search Phrase Found"; 
+					response.url = ""; 
+		        } else {
+					try {
+						List<SearchResult> searchResults = searchFiles.query(phrase);
+						if ( searchResults.size() > 0 ) {
+							// Create the plain text output
+							SearchResult searchResult = searchResults.get(0);
+//						    PingSEP pingSEP = new PingSEP(searchResult.url);  
+//							new Thread(pingSEP).start();
+							
+							response.subject = "Results for " + phrase + ".";
+							response.preamble = searchResult.preamble;
+							response.url = "https://plato.stanford.edu/archives/win2016/"+searchResult.url;
+						} else {
+							response.subject = "Search for " + phrase + ".";
+							response.preamble = "Sorry, nothing found for " + phrase + ".";
+//							log.info("No results for " + searchPhrase);
+						}
+					} catch (ParseException | IOException e) {
+						response.subject = "Results for " + phrase + ".";
+						response.preamble = e.getLocalizedMessage();
+						response.url = "";
+					}
+		        }
+				
+				if (response.preamble.length() > 290 ) {
+					response.preamble = response.preamble.substring(0,  290); 					
+				}
+				objectMapper.writeValue(output, response);
+
 			} else {
-				objectMapper.writeValue(output, command);
-			}
+				objectMapper.writeValue(output, params);
+			}		
 		} catch (JsonParseException e) {
 			try {
 				output.write(e.getLocalizedMessage().getBytes(StandardCharsets.UTF_8));
@@ -60,16 +111,17 @@ public class LambdaFunctionHandler implements RequestStreamHandler {
 	public static void main(String... strings) throws Exception {
 //		String input = "{\"action\":\"quote\", \"query\":\"frege\"}";
 //		String input = "{\"action\":\"quote\"}";
-		String input = "{'action':'quote'}";
+//		String input = "{'action':'quote'}";
+//		String input = new String(Files.readAllBytes(Paths.get(LambdaFunctionHandler.class.getResource("/input.txt").toURI())));
 		ByteArrayOutputStream output = new ByteArrayOutputStream(); 
 		new LambdaFunctionHandler().handleRequest( 
-			new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8)), 
+			new ByteArrayInputStream(Files.readAllBytes(Paths.get(LambdaFunctionHandler.class.getResource("/input.txt").toURI()))), 
 			output, 
 			null
 		);
 		System.out.println(output.toString());
 	}
-	
+/*	
 	public static class RequestCommand {
 		public String action;
 		public String query;
@@ -78,7 +130,7 @@ public class LambdaFunctionHandler implements RequestStreamHandler {
 			return "[" + action + ", " + query + "]";
 		}
 	}
-		
+*/		
 	public static class QuoteResponse {
 		public String subject;
 		public String preamble;
--- src/test/java/com/amazonaws/lambda/sep/LambdaFunctionHandlerTest.java
+++ src/test/java/com/amazonaws/lambda/sep/LambdaFunctionHandlerTest.java
@@ -5,6 +5,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,21 +17,58 @@ import org.junit.Test;
  */
 public class LambdaFunctionHandlerTest {
 
-    private static final String SAMPLE_INPUT_STRING = "{\"action\": \"test\"}";
-    private static final String EXPECTED_OUTPUT_STRING = "{\"action\":\"test\",\"query\":null}";
+    private static final String EXPECTED_OUTPUT_STRING = "{\"path\":{},\"querystring\":{\"action\":\"test\"},\"header\":{}}";
+    private static final String EXPECTED_OUTPUT_SEARCH_VALID = 
+    		"{\"subject\":\"Results for frege.\",\"preamble\":\"Friedrich Ludwig Gottlob Frege (b. 1848, d. 1925) was a German mathematician, logician, and philosopher who worked at the University of Jena. Frege essentially reconceived the discipline of logic by constructing a formal system which, in effect, constituted the first ‘predicate calculus’. \",\"url\":\"https://plato.stanford.edu/archives/win2016/entries/frege/\"}";
+    private static final String EXPECTED_OUTPUT_SEARCH_INVALID = "{\"subject\":\"Search for xxx.\",\"preamble\":\"Sorry, nothing found for xxx.\",\"url\":null}";
+    private LambdaFunctionHandler handler = new LambdaFunctionHandler();
+
 
     @Test
-    public void testLambdaFunctionHandler() throws IOException {
-        LambdaFunctionHandler handler = new LambdaFunctionHandler();
+    public void testTest() throws IOException, URISyntaxException {
 
-        InputStream input = new ByteArrayInputStream(SAMPLE_INPUT_STRING.getBytes());;
+        InputStream input = new ByteArrayInputStream(Files.readAllBytes(Paths.get(LambdaFunctionHandlerTest.class.getResource("/inputTest.txt").toURI())));
         OutputStream output = new ByteArrayOutputStream();
 
         handler.handleRequest(input, output, null);
 
-        // TODO: validate output here if needed.
         String sampleOutputString = output.toString();
-        System.out.println(sampleOutputString);
         Assert.assertEquals(EXPECTED_OUTPUT_STRING, sampleOutputString);
     }
+    @Test
+    public void testSearchValid() throws IOException, URISyntaxException {
+
+        InputStream input = new ByteArrayInputStream(Files.readAllBytes(Paths.get(LambdaFunctionHandlerTest.class.getResource("/inputSearchValid.txt").toURI())));
+        OutputStream output = new ByteArrayOutputStream();
+
+        handler.handleRequest(input, output, null);
+
+        String sampleOutputString = output.toString();
+        Assert.assertEquals(EXPECTED_OUTPUT_SEARCH_VALID, sampleOutputString);
+    }
+    @Test
+    public void testSearchInvalid() throws IOException, URISyntaxException {
+
+        InputStream input = new ByteArrayInputStream(Files.readAllBytes(Paths.get(LambdaFunctionHandlerTest.class.getResource("/inputSearchInvalid.txt").toURI())));
+        OutputStream output = new ByteArrayOutputStream();
+
+        handler.handleRequest(input, output, null);
+
+        String sampleOutputString = output.toString();
+        Assert.assertEquals(EXPECTED_OUTPUT_SEARCH_INVALID, sampleOutputString);
+    }
+    @Test
+    public void testQuote() throws IOException, URISyntaxException {
+
+        InputStream input = new ByteArrayInputStream(Files.readAllBytes(Paths.get(LambdaFunctionHandlerTest.class.getResource("/inputQuote.txt").toURI())));
+        OutputStream output = new ByteArrayOutputStream();
+
+        handler.handleRequest(input, output, null);
+
+        String sampleOutputString = output.toString();
+        Assert.assertTrue("Excepted Subject", sampleOutputString.contains("subject"));
+        Assert.assertTrue("Excepted Preamble", sampleOutputString.contains("preamble"));
+        Assert.assertTrue("Excepted URL", sampleOutputString.contains("url"));
+    }
+
 }
